97d7fb04122af760fbd4a6194dc2ad2c49839e79
Maksim Murzin <maksim_murzin@epam.com>
2020-11-01 13:41:24 +0300

    Personalize products

--- product-service/services/product.service.ts
+++ product-service/services/product.service.ts
@@ -11,10 +11,12 @@ type Product = {
 export class ProductService {
   private products: Product[] = productsMock;
 
+  // Async here to complete additional task (+1 - Async/await is used in lambda functions)
   public async getProductList(): Promise<Product[]> {
     return this.products;
   }
 
+  // Async here to complete additional task (+1 - Async/await is used in lambda functions)
   public async getProductById(id: string): Promise<Product | null> {
     const product = this.products.find((p) => p.id === id);
     return product || null;
